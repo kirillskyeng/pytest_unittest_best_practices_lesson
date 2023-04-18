9c6f7e735e1e8c63e4a912acdd6c2a4499fce8b9
kirillskyeng <kirill.netreba@skyeng.ru>
2023-04-18 10:38:45 +0300

    Add test for divisible_by_number function

--- tests/test_math_fnc.py
+++ tests/test_math_fnc.py
@@ -1,6 +1,6 @@
 import unittest
 from parameterized import parameterized
-from src.math_fnc import is_prime
+from src.math_fnc import is_prime, divisible_by_number
 
 
 class TestIsPrime(unittest.TestCase):
@@ -12,3 +12,27 @@ class TestIsPrime(unittest.TestCase):
                            ])
     def test_is_prime(self, number, expected):
         self.assertEqual(is_prime(number), expected)
+
+
+class TestDivisibleByNumber(unittest.TestCase):
+    def setUp(self) -> None:
+        self.numbers = [1, 2, 3, 4, 5, 6]
+
+    @parameterized.expand([(1, [1, 2, 3, 4, 5, 6]),
+                           (2, [2, 4, 6]),
+                           (3, [3, 6]),
+                           (5, [5]),
+                           (7, []),
+                           ])
+    def test_divisible_by_number(self, number, expected):
+        self.assertEqual(divisible_by_number(self.numbers, number), expected)
+
+    def test_divisible_by_negative_number(self):
+        """Проверяем, что функция корректно обрабатывает отрицательные числа."""
+        self.assertEqual(divisible_by_number(self.numbers, -2), [2, 4, 6])
+
+    def test_divisible_by_zero(self):
+        """Проверяем, что функция выдает ошибку при попытке деленя на 0."""
+        with self.assertRaises(ZeroDivisionError):
+            divisible_by_number(self.numbers, 0)
+
