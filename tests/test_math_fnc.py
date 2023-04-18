65f9aaabbd8fcd4a43072adaee1d34482632926c
kirillskyeng <kirill.netreba@skyeng.ru>
2023-04-18 11:18:27 +0300

    Add test for divisible_by_number function (pytest)

--- tests/test_math_fnc.py
+++ tests/test_math_fnc.py
@@ -1,5 +1,5 @@
 import pytest
-from src.math_fnc import is_prime
+from src.math_fnc import is_prime, divisible_by_number
 
 
 @pytest.mark.parametrize('number, expected', [(0, False),
@@ -10,3 +10,27 @@ from src.math_fnc import is_prime
                                               ])
 def test_is_prime(number, expected):
     assert is_prime(number) == expected
+
+
+@pytest.fixture
+def numbers():
+    return [1, 2, 3, 4, 5, 6]
+
+
+@pytest.mark.parametrize('divisor, expected', [(1, [1, 2, 3, 4, 5, 6]),
+                                               (2, [2, 4, 6]),
+                                               (3, [3, 6]),
+                                               (5, [5]),
+                                               (7, []),
+                                               ])
+def test_divisible_by_number(numbers, divisor, expected):
+    assert divisible_by_number(numbers, divisor) == expected
+
+
+def test_divisible_by_negative_number(numbers):
+    assert divisible_by_number(numbers, -2) == [2, 4, 6]
+
+
+def test_divisible_by_zero(numbers):
+    with pytest.raises(ZeroDivisionError):
+        divisible_by_number(numbers, 0)
